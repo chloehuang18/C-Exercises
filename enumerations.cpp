20368dbee55cd1ed660dc3f9261fbb17bb5c427b
Chloe Huang <47125676+chloehuang18@users.noreply.github.com>
2021-04-01 10:42:52 -0600

    Update enumerations.cpp

--- enumerations.cpp
+++ enumerations.cpp
@@ -1,2 +1,30 @@
 
 // An enumeration type declares a name for a new type and possible values for that type.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   enum AcceptedCoins {ADD_QUARTER, ADD_DIME, ADD_NICKEL, ADD_UNKNOWN};
+   int totalDeposit;
+   int userInput;
+
+   totalDeposit = 0;
+
+   cout << "Add coin: 0 (add 25), 1 (add 10), 2 (add 5).  ";
+   cin >> userInput;
+
+   if (userInput == ADD_QUARTER) {
+      totalDeposit = totalDeposit + 25;
+   }
+
+   /* Your solution goes here  */
+
+   else {
+      cout << "Invalid coin selection." << endl;
+   }
+
+   cout << "totalDeposit: " << totalDeposit << endl;
+
+   return 0;
+}
